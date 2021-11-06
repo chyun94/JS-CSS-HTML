230f9fe8e5132ba5a4db9553798d4145da001392
choice <hyunin94@naver.com>
2021-11-06 21:54:51 +0900

    20211106저녁공부

---
+++ 20211106 화살표 함수.js
@@ -0,0 +1,13 @@
+// 화살표 함수
+// 펑션,식별자 빼고 소괄호 중괄호 사이에 => 를 입력해주면 된다. 
+var num1 = 13;
+var num2 = 7;
+// function sum(num1,num2) {
+//     return num1+num2
+// }
+// const result = sum(num1,num2)
+// console.log(result);
+
+(num1,num2)=>{return num1+num2;}
+const result = (num1+num2)*2;
+console.log(result);
