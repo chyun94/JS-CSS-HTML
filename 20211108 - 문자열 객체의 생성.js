c9585d4b97b8ceef9756e82ca0812660d165f421
choice <hyunin94@naver.com>
2021-11-08 15:40:24 +0900

    난수

---
+++ 20211108 - 문자열 객체의 생성.js
@@ -0,0 +1,7 @@
+var hello = new String("HELLO");
+// console.log("위는 문자열 객체 hello를 생성한다");
+var res = hello.concat("JS");   
+// console.log("res 변수에 hello문자열 객체에다가 concat함수(재료'JS')를 실행한 결과를 대입한다.");
+console.log(res);
+console.log(hello.length);
+console.log(res.length);
